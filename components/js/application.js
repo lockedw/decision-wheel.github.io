fd602425ac3a6dd9523f32ab61332215eae3a027
lockedw <104690046+lockedw@users.noreply.github.com>
2022-10-16 20:49:23 +0300

    Everything work nice

--- components/js/application.js
+++ components/js/application.js
@@ -2,18 +2,32 @@ const inpDec = document.getElementById("enterDecide");
 const decidebutton = document.getElementById("decButton");
 const addDec = document.getElementById("addDecide");
 var decisionlist = document.querySelector(".inputlar");
+var remitem = document.getElementById("removeitem")
+
+
+
+
+
+
 var deger = inpDec.value;
+var decision1 = document.querySelector("#Karar1")
+var decision2 = document.querySelector("#Karar2")
+var decision3 = document.querySelector("#Karar3")
+var decision4 = document.querySelector("#Karar4")
+
+
+
+
+
+
+
+defid = 1;
 
 eventListeners();
-
-
-
-
-
-
-
 function eventListeners(){
     addDec.addEventListener('click', countClicks);
+    remitem.addEventListener('click', remove);
+    decidebutton.addEventListener('click', random)
 }
 
 var clickLimit = 5;
@@ -29,6 +43,9 @@ function countClicks() {
   }
 }
 
+function plus(){
+    console.log("ads")
+}
 
 
 
@@ -41,25 +58,21 @@ function addDecide() {
         value = inpDec.value;
         dec.value = value;
         console.log(typeof dec);
-        
-        
         dec.className = `Karar${defid}`
-
-
-
+        defid += 1;
         inpDec.value = "";
 }
+function remove() {
+    
+}
+const decitions = [decision1,decision2,decision3,decision4];
 
+function random(e) {
+    const random = Math.floor(Math.random() * decitions.length);
+    console.log(random, decitions[random]);
+    var result = document.querySelector(".resultbox").innerHTML=random;
+    return(e);
+}
 
-
-event1 = "emo";
-event2 = "erdal";
-event3 = "arda"
-
-
-
-
-
-const decitions = [event1,event2,event3];
-const random = Math.floor(Math.random() * decitions.length);
-console.log(random, decitions[random]);
+/* const random = Math.floor(Math.random() * decitions.length);
+console.log(random, decitions[random]); */
