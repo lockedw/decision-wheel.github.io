e36ec31fae13c94d179b5050dcde4cb5ffc79dd3
lockedw <104690046+lockedw@users.noreply.github.com>
2022-10-16 16:54:33 +0300

    i try something

--- components/js/application.js
+++ components/js/application.js
@@ -1,22 +1,47 @@
 const inpDec = document.getElementById("enterDecide");
 const decidebutton = document.getElementById("decButton");
 const addDec = document.getElementById("addDecide");
+var deger = inpDec.value;
 
-addDec.addEventListener('click', function(){
-    console.log("ads")
-});
+eventListeners();
 
+
+
+
+
+
+
+function eventListeners(){
+    addDec.addEventListener('click', addDecide);
+}
+
+
+
+
+
+
+
+function addDecide() {
+
+        inputlar = document.querySelector(".inputlar")
+        dec = document.createElement("input");
+        inputlar.appendChild(dec);
+        value = inpDec.value;
+        dec.value = value;
+        console.log(typeof dec);
+        inpDec.value = "";
+}
 
 
 
 event1 = "emo";
 event2 = "erdal";
-
-
-const decitions = [event1,event2];
-
+event3 = "arda"
 
 
 
+
+
+const decitions = [event1,event2,event3];
 const random = Math.floor(Math.random() * decitions.length);
 console.log(random, decitions[random]);
